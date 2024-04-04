ef0f8e8d1d23cbf90aa3dec93e343c9bbab76e55
Damageddream <marcan.grab@gmail.com>
2024-04-04 09:25:57 +0200

    first commit

---
+++ src/test/java/com/damageddream/MedicalClinicProxy/MedicalClinicProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damageddream.MedicalClinicProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
