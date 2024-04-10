7c8771838f3441d9c79a6a0b7974a18a2988847d
Damageddream <marcan.grab@gmail.com>
2024-04-10 17:24:11 +0200

    MED-237

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-FROM ubuntu:latest
-LABEL authors="marca"
-
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:17-jdk-alpine
+COPY target/Medical-Clinic-Proxy-0.0.1-SNAPSHOT.jar Medical-Clinic-Proxy-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/Medical-Clinic-Proxy-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/damageddream/MedicalClinicProxy/remote/MedicalClinicClient.java
+++ src/main/java/com/damageddream/MedicalClinicProxy/remote/MedicalClinicClient.java
@@ -9,7 +9,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//nazwac z myslnikami
+//dodac volumy do pgadmina
 @FeignClient(name = "MedicalClinicClient",
         url = "${feign.client.MedicalClinicClient.url}",
         configuration = ClientConfiguration.class
